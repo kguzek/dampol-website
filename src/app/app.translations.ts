01832cf093029dc0f43fc422d227297396b350b6
Konrad (PC) <konrad@guzek.uk>
2023-07-16 18:39:33 +0200

    reorganise project assets, improve accessibility

--- src/app/app.translations.ts
+++ src/app/app.translations.ts
@@ -7,11 +7,13 @@ export type LanguageCode = keyof typeof LANGUAGE_CODES;
 
 export const TRANSLATIONS = {
   gb: {
+    alt: { dampolLogo: 'Dampol logo' },
     navbar: { about: 'About', contact: 'Contact', products: 'Products' },
   },
   dk: {
+    alt: { dampolLogo: 'Dampol logo' },
     navbar: { about: 'Om os', contact: 'Kontakt', products: 'Produkter' },
   },
 };
 
-export type Translation = typeof TRANSLATIONS.gb;
+export type Translation = typeof TRANSLATIONS.gb & typeof TRANSLATIONS.dk;
